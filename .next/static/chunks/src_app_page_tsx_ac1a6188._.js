c49a91c4a7479d8370bbba123950e8ddac5cd987
Franklin <franklinbarbs763@gmail.com>
2025-04-05 06:45:43 -0300

    feat(modal-ne-info): adds new modal to allow create some infos

--- .next/static/chunks/src_app_page_tsx_ac1a6188._.js
+++ .next/static/chunks/src_app_page_tsx_ac1a6188._.js
@@ -5,8 +5,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/src_app_page_tsx_ac1a6188._.js",
   "chunks": [
-    "static/chunks/node_modules_884afd30._.js",
-    "static/chunks/src_app_2ca3549a._.js"
+    "static/chunks/node_modules_75bc5dd0._.js",
+    "static/chunks/src_app_7cb6fc09._.js"
   ],
   "source": "dynamic"
 });
