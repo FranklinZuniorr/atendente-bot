3adeeddc9d85e0d1e30b91bf87941156e5dc644c
Franklin Vieira <franklinbarbs763@gmail.com>
2025-12-30 16:58:27 -0300

    chore(evolution): downgrade

--- src/app/api/infra/mongoDb/index.ts
+++ src/app/api/infra/mongoDb/index.ts
@@ -1,5 +1,5 @@
-import { ENVS } from '@/constants';
 import mongoose from 'mongoose';
+import { ENVS } from '@/constants';
 
 const MONGO_URI = ENVS.mongoUrl;
 
@@ -7,9 +7,13 @@ if (!MONGO_URI) {
   throw new Error('⚠️ MONGO_URI não foi definida no .env');
 }
 
+let isConnected = false;
+
 export const connectDB = async (): Promise<void> => {
-  if (mongoose.connection.readyState >= 1) {
+  if (isConnected || mongoose.connection.readyState === 1) {
+    return;
   }
 
   await mongoose.connect(MONGO_URI);
+  isConnected = true;
 };
